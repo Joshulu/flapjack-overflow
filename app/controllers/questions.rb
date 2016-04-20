04b7022b121c8d3bdd737116f181426d24072eb1
Joshua Lugo <email": null>
2016-04-20 14:46:53 -0500

    About to pull from origin and merge to feature branch

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -2,17 +2,17 @@ get '/' do
   redirect '/questions'
 end
 
-get '/questions' do
-  @questions = Question.all
-  erb :"questions/index"
-end
-
 get "/questions/:id" do
   @question = Question.find(params[:id])
   @user = User.find(@question.creator_id)
   @answers = @question.answers
   @q_comments = @question.responses
   erb :"questions/show"
+end
+
+get '/questions' do
+  @questions = Question.all
+  erb :"questions/index"
 end
 
 post '/questions' do
