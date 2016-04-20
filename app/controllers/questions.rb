66372bc6b6df46383f1c47247181afd63d5805a4
Gregory Toprak <gd.toprak@gmail.com>
2016-04-20 14:16:35 -0500

    Add question creation form and sinatra stuff

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,6 +3,17 @@ get '/' do
 end
 
 get '/questions' do
-  @questions = Questions.all
-  erb :questions
+  @questions = Question.all
+  erb :"/questions/index"
+end
+
+post '/questions' do
+  params[:question][:creator] = User.first ############# current logged in user
+  question = Question.new(params[:question])
+  if question.save
+    redirect '/questions'
+  else
+    @errors = question.errors.full_messages
+    erb :"/questions/index"
+  end
 end
