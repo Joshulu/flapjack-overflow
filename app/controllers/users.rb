b80e402493dc00cc00b0a6a53655072e086b7755
Sean Norton <NortonSeanM@gmail.com>
2016-04-20 13:44:10 -0500

    Add signup and login functionality

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -3,3 +3,25 @@ get '/logout' do
   session.delete(:user_id)
   redirect "/"
 end
+
+# takes you to signup form page
+get '/users/new' do
+  erb :"users/new"
+end
+
+# creates new user with post request
+post '/users' do
+  new_user = User.create(username: params[:username], password: params[:password])
+  session[:id] = new_user.id
+  redirect '/'
+end
+
+#** logs in user need error messages
+get '/login' do
+  user = User.find_by(username: params[:username])
+  if user.password == params[:password]
+    session[:id] = user.id
+    erb :"users/login"
+  end
+  redirect '/'
+end
