74e99e6d6b90ddc212e20bfed600fd840bb38555
Joshua Lugo <email": null>
2016-04-21 15:26:12 -0500

    Vote buttons work to save new vote objects, but need JS to load without redirect

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-  $(".vote-button").on("click", "button", function() {
+  $(".vote_box").on("submit", "form", function() {
     event.preventDefault();
     var $button = $(this);
-    var vote_type = $button.attr('value')
+    var data = $button.serialize();
+    var verb = $button.attr("method");
+    var route = $button.attr("action");
+
+    var request = $.ajax({
+      method: verb,
+      url: route,
+      data: data,
+    });
+    request.done(function(msg){
+      console.log(msg);
+    });
+    var vote_type = $button.attr('value');
     if (vote_type == "upvote") {
-
     } else if (vote_type == "downvote") {
-
     } else {
 
     };
