be7dc60fa2a00d87460d8b4a63718427b4bdf8e0
Gregory Toprak <gd.toprak@gmail.com>
2016-04-22 09:05:06 -0500

    downvotes now work

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,30 +1,8 @@
-# post '/up_vote' do
-#   Vote.new(voter_id: session[:user_id], votable_id: params[:id] , votable_type: , vote_type: )
-# end
-
-post '/questions/:id/vote' do
-
-end
-
-post '/answers/:id/vote' do
-
-end
-
-post '/comments/:id/vote' do
-
-end
 
 post '/votes' do
   object_type = params['object_type']
   object_id = params['object_id'].to_i
   vote_type = params['vote_type']
-
-  puts
-  puts
-  puts object_id
-  puts object_type
-  puts vote_type
-  puts
 
   case object_type
   when "Question"
@@ -37,11 +15,9 @@ post '/votes' do
 
   Vote.create(voter: current_user,  vote_type: vote_type, votable_type: object_type, votable: object)
 
-
-
   if request.xhr?
     content_type :json
-    { :object_type => object_type, :vote_type => vote_type, :count => object.votes.count.to_s }.to_json
+    { :object_type => object_type, :vote_type => vote_type, :count => object.score.to_s }.to_json
   else
     redirect "/questions/#{object_id}"
   end
--- app/models/response.rb
+++ app/models/response.rb
@@ -3,4 +3,10 @@ class Response < ActiveRecord::Base
   belongs_to :respondable, polymorphic: true
 
   has_many :votes, as: :votable
+
+  def score
+    upvotes = Vote.where(vote_type: "up", votable_type: "Response", votable_id: id)
+    downvotes = Vote.where(vote_type: "down", votable_type: "Response", votable_id: id)
+    return  upvotes.count - downvotes.count
+  end
 end
